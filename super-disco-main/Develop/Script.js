f89a07c7623ac19f26ca576e4c15176e62d7b559
Monica-Claire <monicaclairewallace@gmail.com>
2022-05-06 19:29:00 -0400

    psudo code

---
+++ super-disco-main/Develop/Script.js
@@ -0,0 +1,12 @@
+//display current day on calendar
+moment().format('MMMM Do YYYY, h:mm:ss a');
+//display timeblocks from 8am - 6pm 
+
+//timeblocks are editable
+.data()
+//timeblocks change color depending on if they are past, current or future time
+//if current time is not equal to time, then fade 
+.fadeTo()
+//save button to save details added (is it possible to auto save?)
+
+//data is stored in local storage so it persists if page is refreshed
